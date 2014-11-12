97e9e612c3bd1cf78cb15419f19bf4c53037020a
Yashar Eli <eli@spoteam.com>
2014-11-12 23:44:00 +0200

    removing main activity fragment

--- Time/src/com/time/Activity/MainActivity.java
+++ Time/src/com/time/Activity/MainActivity.java
@@ -1,14 +1,9 @@
 package com.time.Activity;
 
 import com.time.R;
-import com.time.R.id;
-import com.time.R.layout;
-import com.time.R.menu;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -20,22 +15,5 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+   
 }
